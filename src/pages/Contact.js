e32e6ddb83953048de0d04e113d2ef17890f3196
Zana Memic <zanamemic@icloud.com>
2022-02-15 22:38:59 +0100

    updated files

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,21 +8,21 @@ const Contact = () => {
         </h4>
         <div className='container'>
           <div className='row gx-5 justify-content-center align-items-center'>
-            <div className='col-1 d-flex justify-content-center align-items-center' style={{marginRight: '2rem'}}>
+            <div className='col-1 d-flex justify-content-center align-items-center'>
             <a href='https://github.com/Zana994' target={'_blank'} aria-label='Github profile' rel="noreferrer">
               <div className='option-1'>
                 <span></span>
               </div>
             </a>
             </div>
-            <div className='col-1 d-flex justify-content-center align-items-center' style={{marginRight: '2rem'}}>
+            <div className='col-1 d-flex justify-content-center align-items-center'>
             <a href='https://www.linkedin.com/in/zanamemic/' target={'_blank'} aria-label='Linkedin profile' rel="noreferrer">
               <div className='option-2'>
                 <span></span>
               </div>
             </a>
             </div>
-            <div className='col-1 d-flex justify-content-center align-items-center' style={{marginRight: '2rem'}}>
+            <div className='col-1 d-flex justify-content-center align-items-center'>
               <a href='mailto:zana_memic@hotmail.com'>
                 <div className='option-3'>
                   <span></span>
